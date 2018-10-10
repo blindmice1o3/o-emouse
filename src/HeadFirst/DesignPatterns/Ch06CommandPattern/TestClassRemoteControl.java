d08c630ef2d3fc2510c2c6d85ac533ec6faa38ab
jamesgov <blindmice1o3@gmail.com>
2018-10-09 17:46:48 -0700

    docs: Add Ch05 Singleton Pattern (completed)
    Also included the template for Ch06 Command Pattern (just starting)

---
+++ src/HeadFirst/DesignPatterns/Ch06CommandPattern/TestClassRemoteControl.java
@@ -0,0 +1,19 @@
+package HeadFirst.DesignPatterns.Ch06CommandPattern;
+
+/*
+    -----     -----     -----
+    COMMAND pattern: encapsulates a request as an object, thereby letting you parameterize clients with different
+        requests, queue or log requests, and support undoable operation.
+    -----     -----     -----
+
+    //Q. Why use Singleton pattern instead of "instantiation-once" by convention or instead of using a global variable?
+    //A. In many ways, it is a convention (for ensuring one and only one object is instantiated for a given class).
+
+    -----     -----     -----
+    COMMAND pattern: encapsulates a request as an object, thereby letting you parameterize other objects with different
+        requests, queue or log requests, and support undoable operation.
+    -----     -----     -----
+ */
+
+public class TestClassRemoteControl {
+}
