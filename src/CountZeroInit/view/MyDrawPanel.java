aa2f789e69f690cdd2519b3954e6c514dd8fd777
jamesgov <blindmice1o3@gmail.com>
2018-11-03 16:57:41 -0700

    refactor: Remove unused and unneccessary files from MyDrawPanel class. Image file address is now being linked by the GameboyColor gb object's current Map. Though the image file addresses are coming from the Map, I'm actually not using the col and row from the List<Tile> or List<LifeForm> from the Map object, I just hardcoded where images should be displayed on the screen (for now). Getting the col and row fields to determine where each Tile's image file should draw onto the MyDrawPanel is the next crucial step. I need to get the coordinate/movement system (which should be determined by each LifeForm and Tile object's col and row fields) connected with the displaying system (which is currently hardcoded points on the MyDrawPanel).

--- src/CountZeroInit/view/MyDrawPanel.java
+++ src/CountZeroInit/view/MyDrawPanel.java
@@ -1,11 +1,7 @@
 package CountZeroInit.view;
 
 import CountZeroInit.controller.GameboyColor;
-import CountZeroInit.model.creatures.Humanoid;
 import CountZeroInit.model.creatures.LifeForm;
-import CountZeroInit.model.creatures.Monster;
-import CountZeroInit.model.map.Map;
-import CountZeroInit.model.map.MapSpec;
 import CountZeroInit.model.surroundings.Tile;
 
 import javax.swing.*;
@@ -13,47 +9,31 @@ import java.awt.*;
 import java.util.List;
 
 public class MyDrawPanel extends JPanel {
-    //Monster thunderMouse = new ThunderMouse("Winnie", "pooh");
-    //Image thunderMouseImage = new ImageIcon(thunderMouse.getImageAddress()).getImage();
     GameboyColor gb;
 
-    Map map;
-    Monster monster;
-    Image monsterImage;
-    Humanoid humanoid;
-    Image humanoidImage;
+    String monsterImageAddress;
+    String humanoidImageAddress;
 
     public MyDrawPanel(GameboyColor gb) {
+        // The following println() is just to see where MyDrawPanel's constructor is called in the output.
+        System.out.println("MyDrawPanel.constructor...");
+
         this.gb = gb;
-        map = gb.getCurrentMap();
-        monster = (Monster)gb.getCurrentMap().getMapSpec().getLifeFormsOnBoard().get(0);
-        monsterImage = new ImageIcon(monster.getImageAddress()).getImage();
-        humanoid = (Humanoid)gb.getCurrentMap().getMapSpec().getLifeFormsOnBoard().get(1);
-        humanoidImage = new ImageIcon(humanoid.getImageAddress()).getImage();
+
     }
 
     @Override
     public void paintComponent(Graphics g) {
+        // The following println() is just to see where MyDrawPanel's paintComponent() is called in the output.
+        System.out.println("MyDrawPanel.paintComponent()...");
+
         /*
         g.setColor(Color.orange);
         g.fillRect(20, 50, 100, 100);
         */
 
-/*
-        MapSpec mapSpec = map.getMapSpec();
-        List<LifeForm> lifeFormsOnBoard = mapSpec.getLifeFormsOnBoard();
-
-        if (lifeFormsOnBoard.get(0) instanceof Monster) {
-            monster = (Monster)lifeFormsOnBoard.get(0);
-            monsterImage = new ImageIcon(monster.getImageAddress()).getImage();
-        }
-        if (lifeFormsOnBoard.get(1) instanceof Humanoid) {
-            humanoid = (Humanoid)lifeFormsOnBoard.get(1);
-            humanoidImage = new ImageIcon(humanoid.getImageAddress()).getImage();
-        }
-*/
-
         List<Tile> tiles = gb.getCurrentMap().getMapSpec().getTiles();
+        List<LifeForm> lifeFormsOnBoard = gb.getCurrentMap().getMapSpec().getLifeFormsOnBoard();
 
         String imageAddress1 = tiles.get(0).getImageAddress();
         String imageAddress2 = tiles.get(1).getImageAddress();
@@ -61,14 +41,16 @@ public class MyDrawPanel extends JPanel {
 
         String imageAddress4 = tiles.get(3).getImageAddress();
         //String imageAddress5 = tiles.get(4).getImageAddress();
+        monsterImageAddress = lifeFormsOnBoard.get(0).getImageAddress();
         String imageAddress6 = tiles.get(5).getImageAddress();
 
         String imageAddress7 = tiles.get(6).getImageAddress();
         String imageAddress8 = tiles.get(7).getImageAddress();
         //String imageAddress9 = tiles.get(8).getImageAddress();
+        humanoidImageAddress = lifeFormsOnBoard.get(1).getImageAddress();
+
 
         Image image = new ImageIcon(imageAddress1).getImage();
-
         g.drawImage(image,10, 10, this);
         image = new ImageIcon(imageAddress2).getImage();
         g.drawImage(image, 148, 10, this);
@@ -78,7 +60,8 @@ public class MyDrawPanel extends JPanel {
         image = new ImageIcon(imageAddress4).getImage();
         g.drawImage(image, 10, 148,this);
         //image = new ImageIcon(imageAddress5).getImage();
-        g.drawImage(monsterImage, 148, 148, this);
+        image = new ImageIcon(monsterImageAddress).getImage();
+        g.drawImage(image, 148, 148, this);
         image = new ImageIcon(imageAddress6).getImage();
         g.drawImage(image, 286, 148, this);
 
@@ -87,6 +70,7 @@ public class MyDrawPanel extends JPanel {
         image = new ImageIcon(imageAddress8).getImage();
         g.drawImage(image, 148, 286, this);
         //image = new ImageIcon(imageAddress9).getImage();
-        g.drawImage(humanoidImage, 286, 286, this);
+        image = new ImageIcon(humanoidImageAddress).getImage();
+        g.drawImage(image, 286, 286, this);
     }
 }
