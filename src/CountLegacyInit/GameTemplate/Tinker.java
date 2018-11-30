76052f2bde7fd6578f5dee2349a5491da314f2ee
jamesgov <blindmice1o3@gmail.com>
2018-11-30 13:23:19 -0800

    tinker: Gut the application (no more Menu bar or buttons; just JFrame and JPanel) of the class Tinker to try to see why the customdrawing panel isn't visible unless I maximize/restore-down the JFrame. Still haven't gotten this bug fixed, even after ridding it of most of its code (I was really relunctant to let go of what I wrote for Menu bar code, but I figured this is why I have the Tinker class in the first place.... a sandbox experiment area where there should be a lot code deletion).

--- src/CountLegacyInit/GameTemplate/Tinker.java
+++ src/CountLegacyInit/GameTemplate/Tinker.java
@@ -1,6 +1,7 @@
 package CountLegacyInit.GameTemplate;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.image.ImageObserver;
 
@@ -10,98 +11,63 @@ public class Tinker {
     int frameHeight;
     JFrame frame;
     JPanel framePanel;
-    JMenuBar frameMenuBar;
-    JMenu frameBackgroundColorMenu;
-    JMenuItem colorRadioButtonMenuItem;
-
     JPanel displayPanel;
-    JPanel buttonsPanel;
-
     String displayPanelBackgroundImageAddress;
     Image displayPanelBackgroundImage;
-    ImageObserver displayPanelBackgroundImageImageObserver;
-
-
-    JButton upButton;
-    JButton downButton;
-    JButton leftButton;
-    JButton rightButton;
-    JButton aButton;
-    JButton bButton;
-    JButton selectButton;
-    JButton startButton;
 
     public Tinker() {
         initMemberVariables();
-        startTinker();
     }
 
     class DisplayPanel extends JPanel {
         @Override
         public void paintComponent(Graphics g) {
-            g.drawImage(displayPanelBackgroundImage, 0, 0, this.getWidth(), this.getHeight(),
-                    0, 0, displayPanelBackgroundImage.getWidth(displayPanelBackgroundImageImageObserver),
-                    displayPanelBackgroundImage.getHeight(displayPanelBackgroundImageImageObserver), null);
-        }
-    }
-    class DisplayPanelBackgroundImageImageObserver
-            implements ImageObserver {
-        @Override
-        public boolean imageUpdate(Image img, int infoflags, int x, int y, int width, int height) {
-            return false;
+            g.drawImage(displayPanelBackgroundImage, 0, 0,
+                    (int)(framePanel.getWidth()*(0.85)), (int)(framePanel.getHeight()*(0.75)),
+                    0, 0, 1920, 1080, null);
         }
     }
 
     public void initMemberVariables() {
         awt = Toolkit.getDefaultToolkit();
         frameWidth = (int)awt.getScreenSize().getWidth();
-        frameHeight = (int)awt.getScreenSize().getHeight()-38;
-        frame = new JFrame("Tinker");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        framePanel = new JPanel();
+        frameHeight = (int)awt.getScreenSize().getHeight()-38;      // -38 pixels to account for window/start bar
         displayPanelBackgroundImageAddress = "src/MoonRocks_ItsNotWhatYoureThinking/cyberpunk_wallpapers(1920x1080).jpg";
         displayPanelBackgroundImage = awt.getImage(displayPanelBackgroundImageAddress);
-        displayPanelBackgroundImageImageObserver = new DisplayPanelBackgroundImageImageObserver();
-        frameMenuBar = new JMenuBar();
-        frameBackgroundColorMenu = new JMenu("GameboyColor's Color");
-        colorRadioButtonMenuItem = new JRadioButtonMenuItem();
-        frameBackgroundColorMenu.add(colorRadioButtonMenuItem);
-        frameMenuBar.add(frameBackgroundColorMenu);
 
+        frame = new JFrame("Tinker");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(frameWidth, frameHeight);
+        framePanel = new JPanel();
         framePanel.setSize(frameWidth, frameHeight);
+        framePanel.setLayout(null);
         System.out.println("framePanel width&height: " + framePanel.getWidth() + ", " + framePanel.getHeight());
 
         displayPanel = new DisplayPanel();
-        displayPanel.setSize((int)(framePanel.getWidth()*(0.60)), (int)(framePanel.getHeight()*(0.80)));
-        System.out.println("displayPanel width&height: " + displayPanel.getWidth() + ", " + displayPanel.getHeight());
+        displayPanel.setSize((int)(framePanel.getWidth()*(0.85)), (int)(framePanel.getHeight()*(0.75)));
         displayPanel.setLayout(null);
+        displayPanel.setFocusable(true);
+        System.out.println("displayPanel width&height: " + displayPanel.getWidth() + ", " + displayPanel.getHeight());
+
+        framePanel.add(displayPanel);
+        displayPanel.setLocation((framePanel.getWidth()/2)-(displayPanel.getWidth()/2), 50);
+
+        framePanel.setVisible(true);
         displayPanel.setVisible(true);
 
-        buttonsPanel = new JPanel();
-        buttonsPanel.setSize((int)(framePanel.getWidth()*(0.60)),(int)(framePanel.getHeight()*(0.20)));
-        System.out.println("buttonsPanel width&height: " + buttonsPanel.getWidth() + ", " + buttonsPanel.getHeight());
-        buttonsPanel.setLayout(new GridLayout(3, 16));
-        buttonsPanel.add(new JButton("helloWORLD"));
-        buttonsPanel.setVisible(true);
-
-        framePanel.setLayout(null);
-        framePanel.add(displayPanel);
-        displayPanel.setLocation((frameWidth/2)-(displayPanel.getWidth()/2), 1);
-        framePanel.add(buttonsPanel);
-        buttonsPanel.setLocation((frameWidth/2)-(buttonsPanel.getWidth()/2), displayPanel.getHeight()+2);
-        framePanel.setVisible(true);
-
-        frame.setJMenuBar(frameMenuBar);
         frame.setContentPane(framePanel);
+        frame.setVisible(true);
+        frame.repaint();
     }
 
-    public void startTinker() {
-        frame.setVisible(true);
-    }
 
     public static void main(String[] args) {
-        Tinker tinkerer = new Tinker();
 
+        SwingUtilities.invokeLater( new Runnable(){
+            public void run() {
+                Tinker tinkerer = new Tinker();
+            }
+        } );
     }
+
 }
