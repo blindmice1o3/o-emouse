73681dc3e85aba68fc79ee0db9bbd6778ff60ada
jamesgov <blindmice1o3@gmail.com>
2018-12-04 15:29:26 -0800

    refactor: Cleaning up CountLegacyInit's constructor. Encapsulated the menuBar, mainDisplayPanel, secondaryDisplayPanel, and textInputPanel's initialization code into private instance methods and are called within the constructor. The refactoring of the constructor isn't finished. Also, the main() method/entry point is now using an Event Dispatching Thread to instantiate an instance of CountLegacyInit.

--- src/CountLegacyInit/CountLegacyInit.java
+++ src/CountLegacyInit/CountLegacyInit.java
@@ -68,107 +68,13 @@ public class CountLegacyInit extends JFrame
         width = (int)awt.getScreenSize().getWidth();
         height = (int)awt.getScreenSize().getHeight()-38;   // -38  to try to account for start bar
         frameIcon = awt.getImage(frameIconImageAddress);
-        cursorIconMonkey = awt.getImage(cursorIconMonkeyImageAddress);
-        cursorIconPig = awt.getImage(cursorIconPigImageAddress);
-        cursorIconFish = awt.getImage(cursorIconFishImageAddress);
-        cursorMonkey = awt.createCustomCursor(cursorIconMonkey, new Point(0,0), "cursorMonkey");
-        cursorPig = awt.createCustomCursor(cursorIconPig, new Point(0, 0), "cursorPig");
-        cursorFish = awt.createCustomCursor(cursorIconFish, new Point(0, 0), "cursorFish");
 
-        menuBar = new JMenuBar();
-        saveAndLoadMenu = new JMenu("Save/Load");
-        editMenu = new JMenu("Edit");
-        editMenu.setMnemonic(KeyEvent.VK_E);
-        themeMenu = new JMenu("Theme");
+        initCursorIcons();
 
-        saveCommand = new JMenuItem("Record \"current\" state");
-        saveCommand.setToolTipText("save");
-        loadCommand = new JMenuItem("Reboot to \"previous\" state");
-        loadCommand.setToolTipText("load");
-        cutCommand = new JMenuItem(new DefaultEditorKit.CutAction());
-        cutCommand.setText("Cut");
-        cutCommand.setToolTipText("cut (ctrl + x)");
-        cutCommand.setMnemonic(KeyEvent.VK_X);
-        copyCommand = new JMenuItem(new DefaultEditorKit.CopyAction());
-        copyCommand.setText("Copy");
-        copyCommand.setToolTipText("copy (ctrl + c)");
-        copyCommand.setMnemonic(KeyEvent.VK_C);
-        pasteCommand = new JMenuItem(new DefaultEditorKit.PasteAction());
-        pasteCommand.setText("Paste");
-        pasteCommand.setToolTipText("paste (ctrl + v)");
-        pasteCommand.setMnemonic(KeyEvent.VK_V);
-        themeCommand0 = new JMenuItem("ThemeCommand0");
-        themeCommand0.setToolTipText("TODO: implement themeCommand0");
-        themeCommand1 = new JMenuItem("ThemeCommand1");
-        themeCommand1.setToolTipText("TODO: implement themeCommand1");
-        themeRosePetalGlasses = new JMenuItem("ThemeRosePetalGlasses");
-        themeRosePetalGlasses.setToolTipText("from nothing,\nto nothing,\nbut this is everything inbetween");
-        themeRosePetalGlasses.setCursor(cursorFish);
-        // saveCommand.addActionListener(listener);
-        // loadCommand.addActionListener(listener);
-        // themeCommand0.addActionListener(listener);
-        // themeCommand1.addActionListener(listener);
-        saveAndLoadMenu.add(saveCommand);
-        saveAndLoadMenu.addSeparator();
-        saveAndLoadMenu.add(loadCommand);
-        editMenu.add(cutCommand);
-        editMenu.add(copyCommand);
-        editMenu.addSeparator();
-        editMenu.add(pasteCommand);
-        themeMenu.add(themeCommand0);
-        themeMenu.add(themeCommand1);
-        themeMenu.addSeparator();
-        themeMenu.add(themeRosePetalGlasses);
-        menuBar.add(saveAndLoadMenu);
-        menuBar.add(editMenu);
-        menuBar.add(themeMenu);
+        initMenuBar();
         this.setJMenuBar(menuBar);
 
-
-
-        framePanel = new JPanel();
-        framePanel.setPreferredSize(new Dimension(width, height));
-        framePanel.setLayout(new BorderLayout());
-
-        mainDisplayPanel = new BackgroundDisplayPanel((width-(int)(width*0.25)), height-(15 + menuBar.getHeight()));
-        mainDisplayPanel.setPreferredSize( new Dimension((width-(int)(width*0.25)), height-(15 + menuBar.getHeight())));
-        mainDisplayPanel.setBorder(BorderFactory.createLineBorder(Color.YELLOW, 3, false));
-        mainDisplayPanel.setCursor(cursorMonkey);
-
-        secondaryDisplayPanel = new JPanel();
-        secondaryDisplayPanel.setSize( new Dimension((width-(int)(width*0.75)), (height-(15 + menuBar.getHeight()))));
-        secondaryDisplayPanel.setBackground(Color.LIGHT_GRAY);
-        secondaryDisplayPanel.setBorder(BorderFactory.createLineBorder(Color.YELLOW, 2, false));
-        secondaryDisplayPanel.setLayout(new FlowLayout());
-        textOutput = new JTextArea(player1SetNameMessage,38, 28);
-        textOutput.setSize(new Dimension((width-(int)(width*0.75)), (height-(15 + menuBar.getHeight())))); // 15 for the textInput
-        textOutput.setMargin(new Insets(0, 3, 0, 0));
-        textOutput.setLineWrap(true);
-        textOutput.setWrapStyleWord(true);
-        textOutput.setEditable(false);
-        textOutputScrollPane = new JScrollPane(textOutput);
-        textOutputScrollPane.setSize(new Dimension ((width-(int)(width*0.75)), (height-(15 + menuBar.getHeight()))));
-        textOutputScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-        secondaryDisplayPanel.add(textOutputScrollPane);
-        secondaryDisplayPanel.setCursor(cursorPig);
-
-        textInputPanel = new JPanel();
-        textInputPanel.setSize( new Dimension(width, 15));
-        textInputPanel.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY, 2, false));
-        textInputPanel.setLayout(new BorderLayout());
-        textInputLabel = new JLabel(inputMessage);
-        textInput = new JTextField(60);
-        textInput.setMargin(new Insets(0, 3, 0, 0));
-        textInput.setEditable(true);
-        textInput.setFocusable(true);
-        textInput.addActionListener(this);
-        textInputPanel.add(textInputLabel, BorderLayout.LINE_START);
-        textInputPanel.add(textInput, BorderLayout.CENTER);
-
-        framePanel.add(mainDisplayPanel, BorderLayout.CENTER);
-        framePanel.add(secondaryDisplayPanel, BorderLayout.EAST);
-        framePanel.add(textInputPanel, BorderLayout.SOUTH);
-        framePanel.setVisible(true);
+        initFramePanel();
 
         this.setTitle("Moon Rocks - It's NOT What You're Thinking");
         this.setIconImage(frameIcon);
@@ -194,7 +100,21 @@ public class CountLegacyInit extends JFrame
         textInput.requestFocus();
         //textInput.setCaretPosition(inputMessage.length());
 
+    }
 
+    private void initFramePanel() {
+        framePanel = new JPanel();
+        framePanel.setPreferredSize(new Dimension(width, height));
+        framePanel.setLayout(new BorderLayout());
+
+        initMainDisplayPanel();
+        initSecondaryDisplayPanel();
+        initTextInputPanel();
+
+        framePanel.add(mainDisplayPanel, BorderLayout.CENTER);
+        framePanel.add(secondaryDisplayPanel, BorderLayout.EAST);
+        framePanel.add(textInputPanel, BorderLayout.SOUTH);
+        framePanel.setVisible(true);
     }
 
     @Override
@@ -204,25 +124,136 @@ public class CountLegacyInit extends JFrame
         textInput.setText("");
         textInput.requestFocus();
         //if(this.phaseNow == this.phase1) {
-            if (prevRequest.equals("setGreenEggsAndSpam(true);")) {
-                mainDisplayPanel.setSize( new Dimension(50,350) );
-                framePanel.repaint();
-                this.phaseNow = this.phase2;
-            } else if (prevRequest.equals("eggsAreNotSupposeToBeGreen();")) {
-                mainDisplayPanel.setSize( new Dimension(350,50) );
-                framePanel.repaint();
-                this.phaseNow = this.phase0;
-
-            } else {
-                textOutput.append("INPUT ERROR, may only choose from the earlier two options. \n\n\n");
-                textInput.setText("");
-                textInput.requestFocus();
-            }
-       // }
+        if (prevRequest.equals("setGreenEggsAndSpam(true);")) {
+            mainDisplayPanel.setSize( new Dimension(50,350) );
+            this.phaseNow = this.phase2;
+        } else if (prevRequest.equals("eggsAreNotSupposeToBeGreen();")) {
+            mainDisplayPanel.setSize( new Dimension(350,50) );
+            this.phaseNow = this.phase0;
+        } else {
+            textOutput.append("INPUT ERROR, may only choose from the earlier two options. \n\n\n");
+            textInput.setText("");
+            textInput.requestFocus();
+        }
+        // }
     }
 
     public static void main(String[] args) {
-        CountLegacyInit countLegacyInit = new CountLegacyInit();
+        SwingUtilities.invokeLater( new Runnable() {
+            public void run() {
+                CountLegacyInit countLegacyInit = new CountLegacyInit();
+            }
+        } );
+
+    }
+
+    private void initMainDisplayPanel() {
+        mainDisplayPanel = new BackgroundDisplayPanel((width-(int)(width*0.25)), height-(15 + menuBar.getHeight()));
+        mainDisplayPanel.setPreferredSize( new Dimension((width-(int)(width*0.25)), height-(15 + menuBar.getHeight())) );
+        mainDisplayPanel.setBorder(BorderFactory.createLineBorder(Color.YELLOW, 3, false));
+        mainDisplayPanel.setCursor(cursorMonkey);
+    }
+    private void initSecondaryDisplayPanel() {
+        secondaryDisplayPanel = new JPanel();
+        secondaryDisplayPanel.setSize( new Dimension((width-(int)(width*0.75)), (height-(15 + menuBar.getHeight()))));
+        secondaryDisplayPanel.setBackground(Color.LIGHT_GRAY);
+        secondaryDisplayPanel.setBorder(BorderFactory.createLineBorder(Color.BLUE, 2, false));
+        secondaryDisplayPanel.setLayout(new FlowLayout());
+        textOutput = new JTextArea(player1SetNameMessage,38, 28);
+        textOutput.setSize(new Dimension((width-(int)(width*0.75)), (height-(15 + menuBar.getHeight())))); // 15 for the textInput
+        textOutput.setMargin(new Insets(0, 3, 0, 0));
+        textOutput.setLineWrap(true);
+        textOutput.setWrapStyleWord(true);
+        textOutput.setEditable(false);
+        textOutputScrollPane = new JScrollPane(textOutput);
+        textOutputScrollPane.setSize(new Dimension ((width-(int)(width*0.75)), (height-(15 + menuBar.getHeight()))));
+        textOutputScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        secondaryDisplayPanel.add(textOutputScrollPane);
+        secondaryDisplayPanel.setCursor(cursorPig);
+    }
+    private void initTextInputPanel() {
+        textInputPanel = new JPanel();
+        textInputPanel.setSize( new Dimension(width, 15));
+        textInputPanel.setBorder(BorderFactory.createLineBorder(Color.RED, 2, false));
+        textInputPanel.setLayout(new BorderLayout());
+        textInputLabel = new JLabel(inputMessage);
+        textInput = new JTextField(60);
+        textInput.setMargin(new Insets(0, 3, 0, 0));
+        textInput.setEditable(true);
+        textInput.setFocusable(true);
+        textInput.addActionListener(this);
+        textInputPanel.add(textInputLabel, BorderLayout.LINE_START);
+        textInputPanel.add(textInput, BorderLayout.CENTER);
+    }
+
+    private void initCursorIcons() {
+        cursorIconMonkey = awt.getImage(cursorIconMonkeyImageAddress);
+        cursorIconPig = awt.getImage(cursorIconPigImageAddress);
+        cursorIconFish = awt.getImage(cursorIconFishImageAddress);
+        cursorMonkey = awt.createCustomCursor(cursorIconMonkey, new Point(0,0), "cursorMonkey");
+        cursorPig = awt.createCustomCursor(cursorIconPig, new Point(0, 0), "cursorPig");
+        cursorFish = awt.createCustomCursor(cursorIconFish, new Point(0, 0), "cursorFish");
+    }
+
+    private void initMenuBar() {
+        menuBar = new JMenuBar();
+
+        initSaveAndLoadMenu();
+        initEditMenu();
+        initThemeMenu();
+
+        menuBar.add(saveAndLoadMenu);
+        menuBar.add(editMenu);
+        menuBar.add(themeMenu);
+    }
+
+    private void initSaveAndLoadMenu() {
+        saveAndLoadMenu = new JMenu("Save/Load");
+        saveCommand = new JMenuItem("Record \"current\" state");
+        saveCommand.setToolTipText("save");
+        loadCommand = new JMenuItem("Reboot to \"previous\" state");
+        loadCommand.setToolTipText("load");
+        // saveCommand.addActionListener(listener);
+        // loadCommand.addActionListener(listener);
+        saveAndLoadMenu.add(saveCommand);
+        saveAndLoadMenu.addSeparator();
+        saveAndLoadMenu.add(loadCommand);
+    }
+    private void initEditMenu() {
+        editMenu = new JMenu("Edit");
+        editMenu.setMnemonic(KeyEvent.VK_E);
+        cutCommand = new JMenuItem(new DefaultEditorKit.CutAction());
+        cutCommand.setText("Cut");
+        cutCommand.setToolTipText("cut (ctrl + x)");
+        cutCommand.setMnemonic(KeyEvent.VK_X);
+        copyCommand = new JMenuItem(new DefaultEditorKit.CopyAction());
+        copyCommand.setText("Copy");
+        copyCommand.setToolTipText("copy (ctrl + c)");
+        copyCommand.setMnemonic(KeyEvent.VK_C);
+        pasteCommand = new JMenuItem(new DefaultEditorKit.PasteAction());
+        pasteCommand.setText("Paste");
+        pasteCommand.setToolTipText("paste (ctrl + v)");
+        pasteCommand.setMnemonic(KeyEvent.VK_V);
+        editMenu.add(cutCommand);
+        editMenu.add(copyCommand);
+        editMenu.addSeparator();
+        editMenu.add(pasteCommand);
+    }
+    private void initThemeMenu() {
+        themeMenu = new JMenu("Theme");
+        themeCommand0 = new JMenuItem("ThemeCommand0");
+        themeCommand0.setToolTipText("TODO: implement themeCommand0");
+        themeCommand1 = new JMenuItem("ThemeCommand1");
+        themeCommand1.setToolTipText("TODO: implement themeCommand1");
+        themeRosePetalGlasses = new JMenuItem("ThemeRosePetalGlasses");
+        themeRosePetalGlasses.setToolTipText("from nothing,\nto nothing,\nbut this is everything inbetween");
+        themeRosePetalGlasses.setCursor(cursorFish);
+        // themeCommand0.addActionListener(listener);
+        // themeCommand1.addActionListener(listener);
+        themeMenu.add(themeCommand0);
+        themeMenu.add(themeCommand1);
+        themeMenu.addSeparator();
+        themeMenu.add(themeRosePetalGlasses);
     }
 
 }
