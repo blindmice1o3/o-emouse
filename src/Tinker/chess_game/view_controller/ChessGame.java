ce22ed8911fdacd073ba8598d1b52cff044af0b5
jamesgov <blindmice1o3@gmail.com>
2018-12-16 15:47:23 -0800

    feat: Inner-class called ChessBoardMouseListener inside of ChessGame is now able to translate MouseEvent e's int coordinate for getX() and getY() into String that represent rank/file. SOOOOOOOOOOOO much anxiety from messing up with Git. Almost bricked the entire workspace of o-emouse.

--- src/Tinker/chess_game/view_controller/ChessGame.java
+++ src/Tinker/chess_game/view_controller/ChessGame.java
@@ -6,6 +6,8 @@ import Tinker.chess_game.model.token.*;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.List;
 import java.util.Map;
 
@@ -43,7 +45,7 @@ public class ChessGame extends JPanel {
 
         setChessTokenOnChessBoard();
 
-        this.addMouseListener(new ChessBoardMouseListener());
+        this.addMouseListener( new ChessBoardMouseListener() );
         //startGame();
     }
 
@@ -157,3 +159,107 @@ public class ChessGame extends JPanel {
     } // end drawChessBoard(Graphics)
 
 } // end ChessGame class
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class ChessBoardMouseListener implements MouseListener {
+
+    String rankMouseClicked;
+    String fileMouseClicked;
+
+    public ChessBoardMouseListener() {
+        rankMouseClicked = "";
+        fileMouseClicked = "";
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        System.out.println( "Mouse clicked: (" + e.getX() + ", " + e.getY() + "),\n" +
+                "Rank/File: (" + translateMouseClickToRank(e) + "/" + translateMouseClickToFile(e) + ")\n" +
+                "MouseEvent source => " + e.getSource().getClass());
+    }
+
+    public String translateMouseClickToFile(MouseEvent e) {
+        int xMouseClick = e.getX();
+
+        if ( (xMouseClick >= 0) && (xMouseClick < 70) ) {
+            return "A";
+        }
+        else if ( (xMouseClick >= 70) && (xMouseClick < 140) ) {
+            return "B";
+        }
+        else if ( (xMouseClick >= 140) && (xMouseClick < 210) ) {
+            return "C";
+        }
+        else if ( (xMouseClick >= 210) && (xMouseClick < 280) ) {
+            return "D";
+        }
+        else if ( (xMouseClick >= 280) && (xMouseClick < 350) ) {
+            return "E";
+        }
+        else if ( (xMouseClick >= 350) && (xMouseClick < 420) ) {
+            return "F";
+        }
+        else if ( (xMouseClick >= 420) && (xMouseClick < 490) ) {
+            return "G";
+        }
+        else if ( (xMouseClick >= 490) && (xMouseClick < 560) ) {
+            return "H";
+        }
+        else {
+            return "";
+        }
+    }
+
+    public String translateMouseClickToRank(MouseEvent e) {
+        int yMouseClick = e.getY();
+
+        if ( (yMouseClick >= 0) && (yMouseClick < 70) ) {
+            return "8";
+        }
+        else if ( (yMouseClick >= 70) && (yMouseClick < 140) ) {
+            return "7";
+        }
+        else if ( (yMouseClick >= 140) && (yMouseClick < 210) ) {
+            return "6";
+        }
+        else if ( (yMouseClick >= 210) && (yMouseClick < 280) ) {
+            return "5";
+        }
+        else if ( (yMouseClick >= 280) && (yMouseClick < 350) ) {
+            return "4";
+        }
+        else if ( (yMouseClick >= 350) && (yMouseClick < 420) ) {
+            return "3";
+        }
+        else if ( (yMouseClick >= 420) && (yMouseClick < 490) ) {
+            return "2";
+        }
+        else if ( (yMouseClick >= 490) && (yMouseClick < 560) ) {
+            return "1";
+        }
+        else {
+            return "";
+        }
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+}
