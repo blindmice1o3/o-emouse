12f198b2c571f7ab96395b97dbd290c61417ba40
jamesgov <blindmice1o3@gmail.com>
2018-12-17 13:49:45 -0800

    tinker: Tinker with a new package called animation inside Tinker package. There is a JFrame subclass and a JPanel subclass (with a custom paintComponent(Graphics). The frame is just a loader to view the panel and start the timer that controls the panel's animation. The panel displays an Image that represents the background (ground and clouds) that appears to move from right to left. The timer moves the coordinates representing the (window of the camera) one pixel right (so the entire clip of Image gets shifted to the Image's right), unless the right-side of our clip-window reaches the right edge of the Image (in this case 2400 pixel) (when it reaches the end of the Image, the clip-window's coordinate are reset to be the beginning of the Image again... meant to cause a looping of the background).

---
+++ src/Tinker/animation/BackgroundMoving.java
@@ -0,0 +1,70 @@
+package Tinker.animation;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class BackgroundMoving extends JPanel
+        implements ActionListener {
+
+    private final int DELAY = 50;
+    Timer timer;
+
+    String imageAddress = "src/CountZeroInit/model/icons/initBackground.png";
+    ImageIcon ii;
+    Image image;
+    int dx1, dy1, dx2, dy2;
+    int sx1, sy1, sx2, sy2;
+
+    public BackgroundMoving() {
+        this.setSize(600, 400);
+
+        ii = new ImageIcon(imageAddress);
+        image = ii.getImage();
+
+        dx1 = 0;
+        dy1 = 0;
+        dx2 = 600;
+        dy2 = 400;
+
+        sx1 = 0;
+        sy1 = 0;
+        sx2 = 600;
+        sy2 = 400;
+
+        timer = new Timer(DELAY, this);
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        //super.paintComponent(g);
+
+        this.setDoubleBuffered(true);
+
+        drawImage(g);
+    }
+
+    public void drawImage(Graphics g) {
+        g.drawImage(image, dx1, dy1, dx2, dy2, sx1, sy1, sx2, sy2, null);
+        //Toolkit.getDefaultToolkit().sync();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (sx2 < 2400) {
+            sx1++;
+            sx2++;
+        }
+        else {
+            sx1 = 0;
+            sx2 = 600;
+        }
+
+        repaint();
+    }
+
+    public Timer getTimer() {
+        return timer;
+    }
+}
---
+++ src/Tinker/animation/BackgroundMovingJFrame.java
@@ -0,0 +1,28 @@
+package Tinker.animation;
+
+import javax.swing.*;
+
+public class BackgroundMovingJFrame extends JFrame {
+    JPanel panel;
+
+    public static void main(String[] args) {
+        BackgroundMovingJFrame frame = new BackgroundMovingJFrame();
+        frame.start();
+    }
+
+    public BackgroundMovingJFrame() {
+        this.setSize(600, 400);
+        this.setLocationRelativeTo(null);
+        this.setResizable(false);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        panel = new BackgroundMoving();
+        this.setContentPane( panel );
+    }
+
+    public void start() {
+        this.setVisible(true);
+        BackgroundMoving backgroundPanel = (BackgroundMoving)panel;
+        backgroundPanel.getTimer().start();
+    }
+}
