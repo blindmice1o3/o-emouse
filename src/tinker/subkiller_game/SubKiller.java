d0c057136057f1ea741135ed92b7531c6f2c8e94
jamesgov <blindmice1o3@gmail.com>
2018-12-30 20:44:40 -0800

    tinker: Add words to word bank. Start SubKiller game from math.hws.edu javanotes source chapter6 website.

--- src/tinker/subkiller_game/SubKiller.java
+++ src/tinker/subkiller_game/SubKiller.java
@@ -4,6 +4,36 @@ import javax.swing.*;
 
 public class SubKiller extends JPanel {
 
+    public static void main(String[] args) {
 
+        JFrame frame = new JFrame("Submarine Killer");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.setSize(600, 480);
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
+
+        SubKiller panel = new SubKiller();
+        frame.setContentPane(panel);
+
+        frame.setVisible(true);
+
+    } // **** end main(String[]) ****
+
+    // -----------------------------------------------------------------------------------------------------------------
+    // -----------------------------------------------------------------------------------------------------------------
+
+    private Timer timer;
+
+    private int width, height;
+
+    
+
+
+    public SubKiller() {
+
+
+
+    }
 
 } // **** end SubKiller class ****
