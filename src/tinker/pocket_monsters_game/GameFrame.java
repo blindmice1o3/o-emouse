794f79d2716fc290e180e29b77cd17f12c3659d8
jamesgov <blindmice1o3@gmail.com>
2019-01-27 20:24:15 -0800

    tinker: Create new package called pocket_monsters_game. So far there's a JFrame subclass with a main(String[]) to launch the JPanel subclass (GameScreenPanel). GameScreenPanel uses one big image (the entire game map) but only displays a 320 x 320 pixels portion starting the game off at a default location that's specified by x, y int constants. The GameScreenPanel is also a KeyListener. The player uses w, a, s, d to move the 320 x 320 screen-clip around the Image of the game map (with if-statements to keep the player from moving out of bound of the game map's maximum/minimum width and height).

---
+++ src/tinker/pocket_monsters_game/GameFrame.java
@@ -0,0 +1,33 @@
+package tinker.pocket_monsters_game;
+
+import javax.swing.*;
+
+public class GameFrame extends JFrame {
+
+    public static final int GAME_FRAME_WIDTH = 480;
+    public static final int GAME_FRAME_HEIGHT = 600;
+
+    JPanel gameScreenPanel;
+
+    public GameFrame() {
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("Off-brand Pocket Monsters");
+
+        setSize(GAME_FRAME_WIDTH, GAME_FRAME_HEIGHT);
+
+        gameScreenPanel = new GameScreenPanel(GAME_FRAME_WIDTH, GAME_FRAME_HEIGHT);
+        setContentPane( gameScreenPanel );
+
+        setVisible(true);
+
+    } // **** end GameFrame() constructor
+
+    public static void main(String[] args) {
+
+        GameFrame gameFrame = new GameFrame();
+        gameFrame.setLocationRelativeTo(null);
+
+    } // **** end main(String[]) ****
+
+} // **** end GameFrame class ****
---
+++ src/tinker/pocket_monsters_game/GameScreenPanel.java
@@ -0,0 +1,107 @@
+package tinker.pocket_monsters_game;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class GameScreenPanel extends JPanel
+        implements KeyListener {
+
+    public static final int GAME_SCREEN_PANEL_WIDTH = 320;
+    public static final int GAME_SCREEN_PANEL_HEIGHT = 320;
+    public static final int X_START = 960;
+    public static final int Y_START = 3168;
+    public static final int PIXEL_PER_KEY_PRESS = 5;
+
+    private int gameFrameBorderOffsetWidth = 80;
+    private int gameFrameBorderOffsetHeight = 40;
+
+    private int gameFrameWidth;
+    private int gameFrameHeight;
+    private int xCurrent;
+    private int yCurrent;
+
+    Image mapImage;
+    String mapImageAddress = "src/count_zero_init/model/icons/pokemon-gsc-kanto.png";
+
+    /**
+     * No-argument constructor creates an ImageIcon using the image found in the directory specified by mapImageAddress.
+     * Also, the xCurrent and yCurrent (which tracks the top-left corner of the game screen [relative to where it should
+     * be on the file represented by mapImage]) are initialized to pre-specified constants (X_START and Y_START).
+     */
+    public GameScreenPanel(int gameFrameWidth, int gameFrameHeight) {
+
+        this.gameFrameWidth = gameFrameWidth;
+        this.gameFrameHeight = gameFrameHeight;
+        setSize( this.gameFrameWidth, this.gameFrameHeight );
+
+        xCurrent = X_START;
+        yCurrent = Y_START;
+        ImageIcon mapImageIcon = new ImageIcon(mapImageAddress);
+        mapImage = mapImageIcon.getImage();
+
+        setFocusable(true);
+        addKeyListener(this);
+
+    } // **** end GameScreenPanel() constructor ****
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        switch (e.getKeyCode()) {
+            case KeyEvent.VK_W:
+                yCurrent = yCurrent - PIXEL_PER_KEY_PRESS;
+                if (yCurrent < 0) {
+                    yCurrent = 0;
+                }
+                repaint();
+                break;
+            case KeyEvent.VK_A:
+                xCurrent = xCurrent - PIXEL_PER_KEY_PRESS;
+                if (xCurrent < 0) {
+                    xCurrent = 0;
+                }
+                repaint();
+                break;
+            case KeyEvent.VK_S:
+                yCurrent = yCurrent + PIXEL_PER_KEY_PRESS;
+                if (yCurrent > 4320-GAME_SCREEN_PANEL_HEIGHT) {
+                    yCurrent = 4320-GAME_SCREEN_PANEL_HEIGHT;
+                }
+                repaint();
+                break;
+            case KeyEvent.VK_D:
+                xCurrent = xCurrent + PIXEL_PER_KEY_PRESS;
+                if (xCurrent > 4480-GAME_SCREEN_PANEL_WIDTH) {
+                    xCurrent = 4480-GAME_SCREEN_PANEL_WIDTH;
+                }
+                repaint();
+                break;
+        }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+
+    /**
+     * paintComponent(Graphics) is overridden to paint a custom image (specified by mapImageAddress).
+     */
+    @Override
+    public void paintComponent(Graphics g) {
+
+        g.drawImage(mapImage,
+                gameFrameBorderOffsetWidth, gameFrameBorderOffsetHeight,
+                gameFrameBorderOffsetWidth+GAME_SCREEN_PANEL_WIDTH, gameFrameBorderOffsetHeight+GAME_SCREEN_PANEL_HEIGHT,
+                xCurrent, yCurrent, xCurrent+GAME_SCREEN_PANEL_WIDTH, yCurrent+GAME_SCREEN_PANEL_HEIGHT,
+                null);
+
+    } // **** end paintComponent(Graphics) ****
+
+} // **** end GameScreenPanel class ****
