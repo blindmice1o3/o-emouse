22d2c65eac800fde64bc19bd601946e9cfbf43a6
jamesgov <blindmice1o3@gmail.com>
2019-01-30 22:27:44 -0800

    tinker: monopoly_game now has a GUI, it isn't connected to connected to any logical control.

--- src/tinker/monopoly_game/view_controller/MonopolyGame.java
+++ src/tinker/monopoly_game/view_controller/MonopolyGame.java
@@ -1,7 +1,42 @@
 package tinker.monopoly_game.view_controller;
 
-public class MonopolyGame {
+import javax.swing.*;
+import java.awt.*;
 
+public class MonopolyGame extends JFrame {
 
+    private MonopolyFuturamaPanel monopolyFuturamaPanel;
+
+    public MonopolyGame() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(1360, 400);
+        setTitle("Monopoly - Futurama Edition");
+        setLocationRelativeTo(null);
+
+        monopolyFuturamaPanel = new MonopolyFuturamaPanel();
+        setContentPane(monopolyFuturamaPanel);
+
+        setVisible(true);
+    } // **** end MonopolyGame() constructor ****
+
+    public static void main(String[] args) {
+        new MonopolyGame();
+    } // **** end main(String[]) ****
+
+    class MonopolyFuturamaPanel extends JPanel {
+        Image futuramaBoardImage;
+        String futuramaBoardImageAddress;
+
+        public MonopolyFuturamaPanel() {
+            futuramaBoardImageAddress = "src/tinker/monopoly_game/view_controller/monopoly_board-futurama_edition_(1600x1600).jpg";
+            futuramaBoardImage = new ImageIcon(futuramaBoardImageAddress).getImage();
+        } // **** end MonopolyFuturamaPanel() constructor ****
+
+        @Override
+        public void paintComponent(Graphics g) {
+            g.drawImage(futuramaBoardImage, 2, 30, 1340, 330,
+                                            0, 1400, 1600, 1600, null);
+        } // **** end paintComponent(Graphics) ****
+    } // **** end MonopolyFuturamaPanel inner-class ****
 
 } // **** end MonopolyGame class ****
